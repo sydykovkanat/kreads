94a8ef08798c525cd42f966ee129476bee1ef3ad
Kanat Sydykov <sydykov@MacBook-Air-Kanat.local>
2025-03-16 23:53:21 +0600

    add config module for rott is global

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 import { AuthModule } from './auth/auth.module';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PostModule],
+  imports: [
+    UserModule,
+    AuthModule,
+    PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
