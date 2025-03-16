307921a0b73acd2e52c57ac4e7c7d744cb294fe8
Kanat Sydykov <sydykov@MacBook-Air-Kanat.local>
2025-03-17 04:04:47 +0600

    some seo improves

--- frontend/src/app/(root)/page.tsx
+++ frontend/src/app/(root)/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
+
 import { Home } from '@/app/(root)/home';
+
+export const metadata: Metadata = {
+	title: 'Главная',
+};
 
 export default function Page() {
 	return <Home />;
